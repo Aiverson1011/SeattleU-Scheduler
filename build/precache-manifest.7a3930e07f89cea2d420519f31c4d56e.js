1278370bf9aa3a41ea7a369ca4d18b6e8bfb603b
Amanda Iverson <ajohnson764@gmail.com>
2019-06-03 19:40:18 -0700

    add build

---
+++ build/precache-manifest.7a3930e07f89cea2d420519f31c4d56e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4bf5603126c0e2e9086ed6a58e7a859",
+    "url": "/static/media/SU-Redhawk-Icon.b4bf5603.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2c21a58cc9ca4551200",
+    "url": "/static/js/main.f74c1ad3.chunk.js"
+  },
+  {
+    "revision": "ae95afdda03fd3e6f243",
+    "url": "/static/js/2.5da50aa3.chunk.js"
+  },
+  {
+    "revision": "e2c21a58cc9ca4551200",
+    "url": "/static/css/main.7000f2f8.chunk.css"
+  },
+  {
+    "revision": "b32cf78f676192de3f1232c523265154",
+    "url": "/index.html"
+  }
+];
